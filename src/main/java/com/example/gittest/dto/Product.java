0d692471efbfffbe2d9d603c1ed40683d56b5e8e
umeshams <umeshshylendra@gmail>
2023-07-29 16:59:07 +0530

    add geter setter to product

--- src/main/java/com/example/gittest/dto/Product.java
+++ src/main/java/com/example/gittest/dto/Product.java
@@ -6,5 +6,49 @@ public class Product {
 	private String name;
 	private String brand;
 	private Double price;
-	
+
+	public Product() {
+
+	}
+
+	public Product(int id, String name, String brand, Double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.brand = brand;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
 }
